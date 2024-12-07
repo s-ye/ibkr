db43a35425ba215f76c87a1d9f861ed36d1aaf6f
Songyu Ye <songye03@dhcp-vl2041-7998.eduroam.cornell.edu>
2024-12-07 15:59:12 -0500

    weighted unweighted

--- screen.py
+++ screen.py
@@ -8,7 +8,7 @@ from scipy.stats import shapiro, kstest, norm
 import matplotlib.pyplot as plt
 
 
-stock = 'MAR'
+stock = 'UAL'
 
 if __name__ == "__main__":
     backtester = Backtester(stock, 'SMART', 'USD')
